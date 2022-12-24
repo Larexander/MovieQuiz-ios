088ee379b591bbc0624bfd97eb932c5672f4a9ab
Alex Larionov <larry_xander@icloud.com>
2022-12-24 08:40:24 +0600

    Added weak references to "self" in closures

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -105,7 +105,8 @@ final class MovieQuizViewController: UIViewController {
             title: result.title,
             message: result.text,
             preferredStyle: .alert)
-        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self = self else { return }
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
             let firstQuestion = questions[self.currentQuestionIndex]
@@ -140,7 +141,8 @@ final class MovieQuizViewController: UIViewController {
         }
         self.noButton.isEnabled = false
         self.yesButton.isEnabled = false
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self = self else { return }
             self.showNextQuestionOrResults()
             self.noButton.isEnabled = true
             self.yesButton.isEnabled = true
